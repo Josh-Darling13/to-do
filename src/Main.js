888dade0047eb4590ae02baed4790e0ad56317f1
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-20 20:36:57 -0400

    added array for hook

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,33 @@
 import React, { useState } from 'react'
 
 export default function Main() {
-  return (
+
+    const[taskname, settaskname]=useState('');
+    const[tasklist, settasklist]=useState([]);
+
+// console.log(taskname);
+
+function addtask () {
+console.log(taskname);
+    settasklist([...tasklist, taskname])
+}
+
+function deletetask(index){
+    console.log(index);
+}
+
+const tasklistcontent = tasklist.map((task, index)=>{
+    return(
+        <div>
+            <br />
+            {task}
+            <i class="fa fa-trash-alt" onClick={deletetask(index)}></i>
+         </div>
+    )
+})
+
+
+return (
     <div>
         <div className="row justify-content-center">
             <div className="col-md-5 shadow-lg p-3 mb-5 bg-white rounded">
@@ -10,13 +36,18 @@ export default function Main() {
                 type="text"
                 placeholder='enter task'
                 className='form-control'
+                value={taskname}
+                onChange={(e)=>{settaskname(e.target.value)}}
                 />
                 <button
                 className='btn btn-success'
+                onClick={addtask}
                 >
                     ADD
                 </button>
-
+                <p>
+                {tasklistcontent}
+                </p>
 
 
             </div>
